8b1fca01e5fa5bd3b5fe92fea7540fed58926abf
Sanju Srivatsa <71341912+Sanju-srivatsa@users.noreply.github.com>
2021-08-30 18:02:15 +0530

    80 ms

---
+++ solutions/258. Add Digits.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number} num
+ * @return {number}
+ */
+var addDigits = function(num) {
+     let n = num;
+    while (n >= 10) {
+        let sum = 0;
+    while (n > 0) {
+            sum = sum + n % 10;
+            n = Math.floor(n / 10);
+        }
+        n = sum;
+    }
+    return n;  
+};
